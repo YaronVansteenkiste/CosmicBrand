74bb11404cf2fd9e77b1f7e66aa6fd1ce05ba1e1
yaronvsk <yaronvsk@googlemail.com>
2023-03-05 19:19:11 +0100

    05/03

---
+++ src/main/java/be/yaronvansteenkiste/cosmicbrand/CosmicBrandApplication.java
@@ -0,0 +1,13 @@
+package be.yaronvansteenkiste.cosmicbrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicBrandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmicBrandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/yaronvansteenkiste/cosmicbrand/CosmicBrandApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yaronvansteenkiste.cosmicbrand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmicBrandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
