adf38857576d969abd5fec8bda220e3331a4798d
Yaron <yaron.vansteenkiste@telenet.be>
2023-03-05 20:58:42 +0100

    5/03

--- src/main/java/be/yaronvansteenkiste/cosmicbrand/controllers/HomeController.java
+++ src/main/java/be/yaronvansteenkiste/cosmicbrand/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package be.yaronvansteenkiste.cosmicbrand.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String Home() {
+    public String Home(Model model) {
+        String category1 = "Make-up";
+        String category2 = "Wellness";
+        String category3 = "Utensils";
+        model.addAttribute("category1", category1);
+        model.addAttribute("category2", category2);
+        model.addAttribute("category3", category3);
         return "home";
+    }
+
+    @GetMapping("/contact")
+    public String Contact(Model model) {
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String Login(Model model) {
+        return "login";
     }
 }
