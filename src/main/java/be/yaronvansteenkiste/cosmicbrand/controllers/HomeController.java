dafb4b695d0f58382188e55da9307a96d9ce1ba0
yaronvsk <yaronvsk@googlemail.com>
2023-03-06 11:46:34 +0100

    06/03

--- src/main/java/be/yaronvansteenkiste/cosmicbrand/controllers/HomeController.java
+++ src/main/java/be/yaronvansteenkiste/cosmicbrand/controllers/HomeController.java
@@ -3,12 +3,16 @@ package be.yaronvansteenkiste.cosmicbrand.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.*;
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "home"})
     public String Home(Model model) {
         String category1 = "Make-up";
         String category2 = "Wellness";
@@ -22,6 +26,28 @@ public class HomeController {
     @GetMapping("/contact")
     public String Contact(Model model) {
         return "contact";
+    }
+
+    @PostMapping("/account")
+    public String login(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:mem:dbcosmic", "admin", "");
+            String query = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return "redirect:/home";
+            } else {
+                model.addAttribute("error", "User not found.");
+                return "login";
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "error";
+        }
     }
 
     @GetMapping("/login")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       email VARCHAR(255),
+                       password VARCHAR(255)
+);
+
+
+INSERT INTO "USERS" VALUES
+
+                        (1, 'admin@admin.com', 'admin');
